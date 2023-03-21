2e0512e0e7cc3689ba202f8d0dcf64dd70370407
eoKelvio <kelvinsc7@gmail.com>
2023-03-21 17:08:40 -0300

    Projeto Conta Bancaria

---
+++ FormacaoJavaDeveloper/ProjetoContaBancaria/src/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, insira o seu saldo: ");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.");
+    }
+}
